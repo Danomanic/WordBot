296478b8b04e2fc7375eadb1ef8b6a44b845ca75
Danomanic <dan@zfast.co.uk>
2012-01-13 15:50:47 +0000

    Add Mr Robot Picture, and a Debug Console

--- includes/footer.php
+++ includes/footer.php
@@ -2,5 +2,11 @@
 </div>
 <div id="footer">I'm a robot! Version 1.0.3</div>
 </div>
+<div id="robot"><img src="robot.png"></div>
+<?php if($params['site']['dev']) { ?>
+<div id="console">
+<?php echo "DB=" . $params['db']['database']; ?>
+</div>
+<?php } ?>
 </body>
 </html>
